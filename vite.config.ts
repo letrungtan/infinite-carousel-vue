aa648c33c7fd9516b14e487dc9773c7d9e634307
Lê Trung Tấn <letrungtan94@gmail.com>
2025-03-07 18:22:54 +0700

    #Change libcss to css-injected-by-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import libCss from 'vite-plugin-libcss';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), libCss()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: 'src/index.js',
